31da0b9fba688af20469fb5ace68c7fc274782d5
Yuralitv1982 <yuralitv02@gmail.com>
2024-12-23 19:20:28 +0200

    proxy props

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,7 @@
-export default function Section({ title, id, children }) {
+export default function Section({ title, children, ...props }) {
+   //    console.log({ ...props });
    return (
-      <section id={id}>
+      <section {...props}>
          <h2>{title}</h2>
          {children}
       </section>
